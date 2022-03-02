9d7510be508228b46514d145c1998a3be5a5bf7a
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2022-03-02 12:33:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ if os.path.exists(".env"):
     load_dotenv(".env")
     
 # Necessary Vars
-API_ID = int(os.getenv("API_ID", "6"))
-API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-SESSION = os.getenv("SESSION")
+API_ID = int(os.getenv("API_ID", "11572570"))
+API_HASH = os.getenv("API_HASH", "a55354cc32d24a245ada611f9e4e395a)
+SESSION = os.getenv("SESSION","AQBE3ywcCKLVbBxrGhMQt94RkTqExTbf5LPOrp4Z8s2sb5del-zVs9Y_Ohm4llKas0X0uOTpdlo2V3bVahJlSYGB7qnrYfBpzEYhe7QhPhJudMZixMfM2gf5WehYrSVaoUYXIf2xa7Yzyol0coB9n4dMXwD4XlcoDyMNpnSkDJdMJshTWw2LFPoTSNBmrVbgEz4FfoR9IfOUriJfsAHaV9VBEoLnaOeazE8vsHpSirppWC3OMwqmWHk5rUoYHw3A8XgnSXAyhPx0C8NOpgfNPALijJVdG2B4kh89Xjca20cqrumvrdK4PHS83tfxzrU1nsJfqgf204x8ys5-9g-M4Gw9AAAAATEP6OUA")
 HNDLR = os.getenv("HNDLR", "!")
 GROUP_MODE = os.getenv("GROUP_MODE", "True")
 
